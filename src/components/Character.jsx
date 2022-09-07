faa1e3011d71a6d794489cc3e8db38212b0d5ee9
Yuri Leon <yurileon@hotmail.com.br>
2022-09-07 01:11:46 -0300

    feat: details-page template formatting done

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -8,204 +8,154 @@ export default function Character({ url }) {
   }, []);
 
   const characterDetail = (
-    <div className="fadein grid grid-rows-4 grid-cols-3 overflow-hidden bg-semiblack bg-opacity-95 shadow-xl shadow-semiblack rounded-lg mb-[15vh] mt-10 min-h-[500px]">
+    <div className="fadein grid grid-rows-6 grid-cols-4 overflow-hidden bg-semiblack bg-opacity-95 shadow-xl shadow-semiblack rounded-lg mb-[15vh] mt-10 min-h-[500px]">
       <div
-        onClick={() => console.log(data)}
+        onClick={() => console.log(data.skin_color)}
         id="BG"
-        className="row-span-1 sm:row-span-full sm:h-full bg-characterdetail sm:bg-characterdetail-sm lg:bg-characterdetail-lg bg-no-repeat bg-bottom sm:bg-left bg-cover col-span-3 sm:col-span-1 sm:order-3"
+        className="sm:h-full bg-characterdetail sm:bg-characterdetail-sm lg:bg-characterdetail-lg bg-no-repeat bg-bottom sm:bg-left bg-cover col-span-full row-span-1 sm:row-span-full sm:col-span-2 sm:order-2"
       />
 
-      <div className="flex flex-col row-span-3 sm:row-span-full col-span-full sm:col-span-2 overflow-hidden">
-        <h3 className="text-staryellow underline px-3 sm:px-5 py-2 text-[20px] md:text-[30px] text-white text-start font-bold select-none">
+      <div className="text-staryellow flex flex-col row-span-5 sm:row-span-full col-span-full sm:col-span-2 overflow-hidden">
+        <h3 className="text-white font-starjedi px-3 sm:px-5 py-2 text-[20px] md:text-[30px] font-bold select-none">
           Character Details
         </h3>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Name:</dt>
+          <dd className="italic text-end">teste {data.color}</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Homeland:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Birth year:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Gender:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Height:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Weight:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Hair color:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Eye color:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Skin color:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Movies:</dt>
+          <div className="text-end">
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+          </div>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Starship:</dt>
+          <div className="text-end">
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+          </div>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
-        </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
-        </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 sm:gap-4 sm:px-6">
+          <dt className="font-bold">Vehicles:</dt>
+          <div className="text-end">
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+          </div>
         </div>
       </div>
     </div>
   );
 
   const starshipDetail = (
-    <div className="fadein grid grid-rows-4 grid-cols-3 overflow-hidden bg-semiblack bg-opacity-95 shadow-xl shadow-semiblack rounded-lg mb-[15vh] mt-10 min-h-[500px]">
+    <div className="fadein grid grid-rows-6 grid-cols-4 overflow-hidden bg-semiblack bg-opacity-95 shadow-xl shadow-semiblack rounded-lg mb-[15vh] mt-10 min-h-[500px]">
       <div
-        onClick={() => console.log(data)}
+        onClick={() => console.log(data.skin_color)}
         id="BG"
-        className="row-span-1 sm:row-span-full sm:h-full bg-starshipdetail sm:bg-starshipdetail-sm lg:bg-starshipdetail-lg bg-no-repeat bg-bottom sm:bg-left bg-cover col-span-3 sm:col-span-1 sm:order-3"
+        className="bg-starshipdetail sm:bg-starshipdetail-sm lg:bg-starshipdetail-lg bg-no-repeat bg-bottom sm:bg-left bg-cover col-span-full row-span-1 sm:row-span-full sm:col-span-2 sm:order-2"
       />
 
-      <div className="flex flex-col row-span-3 sm:row-span-full col-span-full sm:col-span-2 overflow-hidden">
-        <h3 className="text-staryellow underline px-3 sm:px-5 py-2 text-[20px] md:text-[30px] text-white text-start font-bold select-none">
+      <div className="text-staryellow flex flex-col row-span-5 sm:row-span-full col-span-full sm:col-span-2 overflow-hidden">
+        <h3 className="text-white font-starjedi px-3 sm:px-5 py-2 text-[20px] md:text-[30px] font-bold select-none">
           Starship Details
         </h3>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">Manufacturer</dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Name:</dt>
+          <dd className="italic text-end">teste {data.color}</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Homeland:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Birth year:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Gender:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Height:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Weight:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Hair color:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Eye color:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Skin color:</dt>
+          <dd className="italic text-end">teste</dd>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Movies:</dt>
+          <div className="text-end">
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+          </div>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 sm:gap-4 sm:px-6 border-b border-darkgray">
+          <dt className="font-bold">Starship:</dt>
+          <div className="text-end">
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+          </div>
         </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
-        </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
-        </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
-        </div>
-        <div className="px-3 py-2 grid grid-cols-2 sm:gap-4 sm:px-6 border-b border-darkgray">
-          <dt className="text-lg font-bold text-staryellow">Name</dt>
-          <dd className="text-staryellow text-xl text-start">
-            teste{data.name}
-          </dd>
+        <div className="text-lg px-3 py-2 sm:gap-4 sm:px-6">
+          <dt className="font-bold">Vehicles:</dt>
+          <div className="text-end">
+            <dd className="italic text-end">teste</dd>
+            <dd className="italic text-end">teste</dd>
+          </div>
         </div>
       </div>
     </div>
